41dff50f7b02d340bf023c908406c4a59c9bc406
Sammy Russom <samuelrussom5@gmail.com>
2023-06-01 16:23:30 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -30,3 +30,15 @@ self.addEventListener('fetch', function (e){
         })
     );
 });
+
+self.addEventListener('fetch', function (e){
+    e.respondWith(
+        caches.match(e.request).then(function (r){
+            return r || fetch(e.request).then(function (response){
+                return caches.open(cacheName).then(function (cache){
+                    cache.put(e.request, response.clone());
+                });
+            });
+        })
+    );
+});
